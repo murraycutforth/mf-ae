6949d9d6a856a8f2666dcaba09451f5b6584c826
Murray Connelly Cutforth <mcc4@stanford.edu>
2024-08-14 11:57:25 -0700

    Added new cmd args

--- src/main/main_train.py
+++ src/main/main_train.py
@@ -27,9 +27,9 @@ def main():
         model=model,
         dataset=datasets['train'],
         dataset_val=datasets['val'],
-        train_batch_size=1,
+        train_batch_size=args.batch_size,
         train_lr=3e-4,
-        train_num_epochs=1,
+        train_num_epochs=args.num_epochs,
         adam_betas=(0.9, 0.99),
         save_and_sample_every=10,
         results_folder=outdir,
@@ -68,6 +68,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-dir', type=str, default=local_data_dir(), help='Path to data directory')
     parser.add_argument('--run-name', type=str, default='debug', help='Name of the run')
+    parser.add_argument('--batch-size', type=int, default=32, help='Batch size for training')
+    parser.add_argument('--num-epochs', type=int, default=100, help='Number of epochs to train for')
     return parser.parse_args()
 
 
