3dbc3d03a00d50770f4654533d2052e664456920
Murray Connelly Cutforth <mcc4@stanford.edu>
2025-02-02 18:19:12 -0800

    v16 run scripts

---
+++ scripts/lassen/v16/run_all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+bsub run_training_v16_0.bsub
+bsub run_training_v16_1.bsub
+bsub run_training_v16_2.bsub
+bsub run_training_v16_3.bsub
+bsub run_training_v16_4.bsub
+bsub run_training_v16_5.bsub
+bsub run_training_v16_6.bsub
+echo "All jobs submitted"
