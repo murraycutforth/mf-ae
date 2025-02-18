c27c958edbcf15964bf95c4a69ebae7de96cd5e6
Murray Connelly Cutforth <mcc4@stanford.edu>
2025-02-18 11:55:02 -0800

    HIT repeat, different seed

---
+++ scripts/lassen/v24/run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+bsub run_training_v24_0.bsub
+bsub run_training_v24_1.bsub
+bsub run_training_v24_2.bsub
+bsub run_training_v24_3.bsub
+bsub run_training_v24_4.bsub
+bsub run_training_v24_5.bsub
+bsub run_training_v24_6.bsub
+bsub run_training_v24_7.bsub
+bsub run_training_v24_8.bsub
+echo "All jobs submitted"
