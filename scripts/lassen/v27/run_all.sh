e26932e41d9ef066181181491f2713b37fabd4c0
Murray Connelly Cutforth <mcc4@stanford.edu>
2025-02-21 12:41:16 -0800

    v27: change compression ratio via conv layers only

---
+++ scripts/lassen/v27/run_all.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+bsub run_training_v27_0.bsub
+bsub run_training_v27_1.bsub
+bsub run_training_v27_2.bsub
+bsub run_training_v27_3.bsub
+bsub run_training_v27_4.bsub
+bsub run_training_v27_5.bsub
+bsub run_training_v27_6.bsub
+bsub run_training_v27_7.bsub
+bsub run_training_v27_8.bsub
+bsub run_training_v27_9.bsub
+bsub run_training_v27_10.bsub
+bsub run_training_v27_11.bsub
+bsub run_training_v27_12.bsub
+bsub run_training_v27_13.bsub
+bsub run_training_v27_14.bsub
+bsub run_training_v27_15.bsub
+bsub run_training_v27_16.bsub
+bsub run_training_v27_17.bsub
+bsub run_training_v27_18.bsub
+bsub run_training_v27_19.bsub
+bsub run_training_v27_20.bsub
+bsub run_training_v27_21.bsub
+bsub run_training_v27_22.bsub
+bsub run_training_v27_23.bsub
+echo "All jobs submitted"
