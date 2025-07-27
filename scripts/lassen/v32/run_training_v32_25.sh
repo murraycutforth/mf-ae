a9909b5c2507857754331a43ec63e08684c25d23
Murray Connelly Cutforth <mcc4@stanford.edu>
2025-07-27 13:11:54 -0700

    Hyper-param grid search

---
+++ scripts/lassen/v32/run_training_v32_25.sh
@@ -0,0 +1,5 @@
+source /usr/workspace/cutforth1/anaconda/bin/activate
+export LD_LIBRARY_PATH=/opt/ibm/spectrumcomputing/lsf/10.1.0.10/linux3.10-glibc2.17-ppc64le-csm/lib
+export PYTHONPATH=/usr/WS1/cutforth1/mf-ae
+cd /usr/WS1/cutforth1/mf-ae
+conda run -n genmodel_env accelerate launch ./src/main/main_train.py  --seed 4 --model-type baseline --dim 32 --dataset-type volumetric --num-dl-workers 0 --batch-size 1 --num-epochs 15 --lr 0.0001 --loss mse --dim-mults 1 2 4 8 8 8 --block-type 1 --z-channels 4 --max-samples 25000 --max-train-samples 10000 --data-dir /usr/workspace/cutforth1/data-mf-ae/patched_hit_experiment/SIGNED_DISTANCE_EXACT --weight-decay 0.0001 --act-type relu --run-name interfacial_ae_v32_run_25_datadirSIGNED_DISTANCE_EXACT_lossmse_lr00001_weightdecay00001_acttyperelu
