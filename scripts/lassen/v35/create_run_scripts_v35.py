3fd3da3b5be56f62837e52fd24731ff6fb9211ce
Murray Connelly Cutforth <mcc4@stanford.edu>
2025-07-29 16:46:03 -0700

    Repeats of v24 and v25 with smaller lr

--- scripts/lassen/v35/create_run_scripts_v35.py
+++ scripts/lassen/v35/create_run_scripts_v35.py
@@ -70,6 +70,7 @@ def main():
 def create_param_str(i, combination):
     param_str = "_".join([f"{key}{str(value)}" for key, value in combination.items()])
     param_str = param_str.replace("/usr/workspace/cutforth1/data-mf-ae/", "")
+    param_str = param_str.replace('/', '_')
     param_str = param_str.replace("(", "")
     param_str = param_str.replace(")", "")
     param_str = param_str.replace(" ", "")
